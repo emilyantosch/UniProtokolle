eddd44702aa222a3e67bd07347bd4d9903786006
ericantosch <ericantosch@web.de>
2021-11-26 13:49:53 +0100

    Added RTP

---
+++ RTP1/.auctex-auto/rtp.el
@@ -0,0 +1,63 @@
+(TeX-add-style-hook
+ "rtp"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("tocbibind" "nottoc" "numbib") ("babel" "ngerman") ("fncychap" "Glenn") ("geometry" "includehead" "headheight=17mm" "footskip=\\dimexpr\\headsep+\\ht\\strutbox\\relax" "tmargin=0mm" "bmargin=\\dimexpr17mm+2\\ht\\strutbox\\relax" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../assets/scripts/tex/structure"
+    "report"
+    "rep10"
+    "fontenc"
+    "inputenc"
+    "amsmath"
+    "fancyhdr"
+    "lettrine"
+    "hyperref"
+    "subcaption"
+    "tikz"
+    "cite"
+    "listings"
+    "tocbibind"
+    "babel"
+    "fncychap"
+    "trfsigns"
+    "geometry"
+    "anyfontsize"
+    "xcolor")
+   (LaTeX-add-labels
+    "eq:1"
+    "eq:2"
+    "eq:3"
+    "eq:4"
+    "eq:5"
+    "eq:6"
+    "eq:7"
+    "eq:8")
+   (LaTeX-add-listings-lstdefinestyles
+    "code")
+   (LaTeX-add-xcolor-definecolors
+    "DarkGreenBlue"
+    "LightGreenBlue"
+    "LightOrange"
+    "DarkOrange"
+    "RedOrange"
+    "BrightRed"
+    "DeepBlue"
+    "codegreen"
+    "codegray"
+    "codepurple"
+    "backcolour"))
+ :latex)
+
---
+++ RTP1/rtp.lol
@@ -0,0 +1,4 @@
+\contentsline {lstlisting}{\numberline {1.1}GPIO-Port Konfiguration}{6}{lstlisting.1.1}%
+\contentsline {lstlisting}{\numberline {1.2}timer.c}{7}{lstlisting.1.2}%
+\contentsline {lstlisting}{\numberline {1.3}Timer-Interrupt mithilfe des Timers 2 im 32-bit-mode}{9}{lstlisting.1.3}%
+\contentsline {lstlisting}{\numberline {1.4}Port N Pins gleichzeitig schalten}{11}{lstlisting.1.4}%
