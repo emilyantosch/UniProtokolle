7e8655ab56b328d37d67568f64945164444a8c8f
ericantosch <ericantosch@web.de>
2021-12-14 17:38:55 +0100

    Added comments

---
+++ MPP3/.auctex-auto/mpp3.el
@@ -0,0 +1,61 @@
+(TeX-add-style-hook
+ "mpp3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("tocbibind" "nottoc" "numbib") ("babel" "ngerman") ("geometry" "includehead" "headheight=17mm" "footskip=\\dimexpr\\headsep+\\ht\\strutbox\\relax" "tmargin=0mm" "bmargin=\\dimexpr17mm+2\\ht\\strutbox\\relax" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../assets/scripts/tex/structure"
+    "article"
+    "art10"
+    "circuitikz"
+    "fontenc"
+    "inputenc"
+    "amsmath"
+    "fancyhdr"
+    "lettrine"
+    "hyperref"
+    "subcaption"
+    "tikz"
+    "cite"
+    "listings"
+    "tocbibind"
+    "babel"
+    "geometry"
+    "anyfontsize"
+    "xcolor")
+   (TeX-add-symbols
+    '("Reactivatenumber" 1)
+    "Suppressnumber"
+    "origthelstnumber"
+    "thelstnumber")
+   (LaTeX-add-labels
+    "eq:eins"
+    "eq:zwei")
+   (LaTeX-add-xcolor-definecolors
+    "DarkGreenBlue"
+    "LightGreenBlue"
+    "LightOrange"
+    "DarkOrange"
+    "RedOrange"
+    "BrightRed"
+    "DeepBlue"
+    "codegreen"
+    "codegray"
+    "codepurple"
+    "backcolour")
+   (LaTeX-add-listings-lstdefinestyles
+    "code"))
+ :latex)
+
--- MPP3/stepfnc.c
+++ MPP3/stepfnc.c
@@ -1,21 +1,32 @@
+/*
+Mikroprozessortechnik P3 - Aufgabe 1
+Autoren: Emily Antosch, Florian Tietjen
+Beschreibung: Dieses Programm approximiert den Spannungswert der Spannung UE vom Komparator durch das Treppenverfahren.
+*/
 #include<stdio.h>
 #include<stdint.h>
 #include "/home/akku/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c1294ncpdt.h"
 
+// Prototypes
 void init_port(void);
 void init_adc(void);
 unsigned int read_adc();
 
+
 int main(void){
-    
+    // Initilization of the varibales    
     unsigned short int da_increase = 0;
     unsigned int adc_value = 0;
     unsigned int last_adc_value = da_increase-1;
+    
     while (1)
     {
+        // ADC start
         ADC0_PSSI_R = 0x0001;
+        // Save last value
         last_adc_value = adc_value;
         adc_value = read_adc();
+        // If adc detectes a near zero value
         if(adc_value <= 0.1){
             //GPIO_PORTM_DATA_R = (last_adc_value % 10) | (((last_adc_value / 10) % 10) << 4);
             //GPIO_PORTL_DATA_R = ((last_adc_value / 100) % 10) & 0x03;
@@ -23,7 +34,9 @@ int main(void){
             GPIO_PORTL_DATA_R = (((int)(last_adc_value * 19.53125) / 100) % 10) & 0x03;
             GPIO_PORTL_DATA_R |= 0x04;
         }
+        // If the button is not pressed
         if(GPIO_PORTD_AHB_DATA_R & 0x02 == 1){
+            // Continue operation
             GPIO_PORTK_DATA_R = da_increase;
             da_increase++;
         }
@@ -32,23 +45,24 @@ int main(void){
 
 
 void init_port(void){
-
+    // Wait for clock for GPIO to be ready
     SYSCTL_RCGCGPIO_R |= 0x0E08;
     while((SYSCTL_PRGPIO_R & 0x0E08) == 0){}
     
-
+    // Initilization of the port D with ADC
     GPIO_PORTD_AHB_DEN_R = 0x02;
     GPIO_PORTD_AHB_AFSEL_R |= 0x01;
     GPIO_PORTD_AHB_DEN_R &= ~0x01;
     GPIO_PORTD_AHB_AMSEL_R |= 0x01;
     GPIO_PORTD_AHB_PUR_R = 0x02;
 
+    // Initilization of the port K with the output
     GPIO_PORTK_DEN_R = 0xFF;
     GPIO_PORTK_DIR_R = 0xFF;
-    
+    // Initilization of the port M for the BCD display
     GPIO_PORTM_DEN_R = 0xFF;
     GPIO_PORTM_DIR_R = 0xFF;
-    
+    // Initilization of the port L for the BCD display and trigger pulse
     GPIO_PORTL_DEN_R = 0x07;
     GPIO_PORTL_DIR_R = 0x07;
     GPIO_PORTL_DATA_R = 0x00;
@@ -56,14 +70,19 @@ void init_port(void){
 
 
 void init_adc(void){
+    // Wait for clock for ADC to be ready
     SYSCTL_RCGCADC_R |= 0x01;
     while((SYSCTL_PRADC_R & 0x01) == 0){}
+    // Disable the ADC
     ADC0_ACTSS_R &= ~0x0F;
+    // Set the corresponding Sequencer
     ADC0_SSMUX0_R |= 0x0F;
+    // Set the corresponding Sequencer 0, length = 1
     ADC0_SSCTL0_R |= 0x02;
+    // Enable the ADC
     ADC0_ACTSS_R |= 0x01;
 }
-
+// Unused function for now
 unsigned int read_adc(){
     unsigned int result = 0;
     while((ADC0_SSFSTAT0_R & (1<<8)) == 0){}
