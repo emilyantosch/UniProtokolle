35bb9a54ca40c573ca8c6772640ba8a0e60f0d88
ericantosch <ericantosch@web.de>
2021-12-15 17:08:14 +0100

    Ich hasse MPP

--- MPP3/.auctex-auto/mpp3.el
+++ MPP3/.auctex-auto/mpp3.el
@@ -4,16 +4,16 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("inputenc" "utf8") ("tocbibind" "nottoc" "numbib") ("babel" "ngerman") ("geometry" "includehead" "headheight=17mm" "footskip=\\dimexpr\\headsep+\\ht\\strutbox\\relax" "tmargin=0mm" "bmargin=\\dimexpr17mm+2\\ht\\strutbox\\relax" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "../assets/scripts/tex/structure"
@@ -39,22 +39,6 @@
     '("Reactivatenumber" 1)
     "Suppressnumber"
     "origthelstnumber"
-    "thelstnumber")
-   (LaTeX-add-labels
-    "eq:zwei")
-   (LaTeX-add-xcolor-definecolors
-    "DarkGreenBlue"
-    "LightGreenBlue"
-    "LightOrange"
-    "DarkOrange"
-    "RedOrange"
-    "BrightRed"
-    "DeepBlue"
-    "codegreen"
-    "codegray"
-    "codepurple"
-    "backcolour")
-   (LaTeX-add-listings-lstdefinestyles
-    "code"))
+    "thelstnumber"))
  :latex)
 
--- MPP3/mpp3.lol
+++ MPP3/mpp3.lol
@@ -1,4 +1,1 @@
-\contentsline {lstlisting}{\numberline {1}GPIO-Port Konfiguration}{3}{lstlisting.1}%
-\contentsline {lstlisting}{\numberline {2}timer.c}{4}{lstlisting.2}%
-\contentsline {lstlisting}{\numberline {3}Timer-Interrupt mithilfe des Timers 2 im 32-bit-mode}{6}{lstlisting.3}%
-\contentsline {lstlisting}{\numberline {4}startup$\textunderscore $ccs.c}{9}{lstlisting.4}%
+\contentsline {lstlisting}{\numberline {1}stepfnc.c zur Approximation der Spannung $U_{E}$ mit dem Treppenverfahren}{3}{lstlisting.1}%
