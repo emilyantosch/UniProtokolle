94a56db9fd2cdcb36b1dbdb557e7da706409fde4
ericantosch <ericantosch@web.de>
2021-12-29 13:52:46 +0100

    Added a lot of stuff

---
+++ GEP3/.auctex-auto/gep3.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "gep3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("tocbibind" "nottoc" "numbib") ("babel" "ngerman") ("geometry" "includehead" "headheight=17mm" "footskip=\\dimexpr\\headsep+\\ht\\strutbox\\relax" "tmargin=0mm" "bmargin=\\dimexpr17mm+2\\ht\\strutbox\\relax" "")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "circuitikz"
+    "fontenc"
+    "inputenc"
+    "subcaption"
+    "amsmath"
+    "fancyhdr"
+    "lettrine"
+    "hyperref"
+    "tikz"
+    "cite"
+    "listings"
+    "tocbibind"
+    "babel"
+    "geometry"
+    "anyfontsize"
+    "xcolor")
+   (LaTeX-add-labels
+    "sec:konklusion"))
+ :latex)
+
