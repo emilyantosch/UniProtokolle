1ea1ece39a2d6253a0aca3fdd54b61219e57fca7
ericantosch <ericantosch@web.de>
2022-01-09 18:10:18 +0100

    Finished analysis

--- GEP3/.auctex-auto/gep3.el
+++ GEP3/.auctex-auto/gep3.el
@@ -3,17 +3,6 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("inputenc" "utf8") ("tocbibind" "nottoc" "numbib") ("babel" "ngerman") ("geometry" "includehead" "headheight=17mm" "footskip=\\dimexpr\\headsep+\\ht\\strutbox\\relax" "tmargin=0mm" "bmargin=\\dimexpr17mm+2\\ht\\strutbox\\relax" "")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -42,6 +31,7 @@
     "tab:fifp"
     "fig:31_242"
     "sec:best-des-spann"
+    "eq:3"
     "sec:stromv-im-nennp"
     "sec:einsch-der-prim"
     "fig:einschalt_0d"
@@ -58,20 +48,9 @@
     "sec:best-der-ersatz"
     "sec:best-der-ersatz-1"
     "sec:ermittl-des-wirk"
-    "sec:konklusion")
-   (LaTeX-add-listings-lstdefinestyles
-    "code")
-   (LaTeX-add-xcolor-definecolors
-    "DarkGreenBlue"
-    "LightGreenBlue"
-    "LightOrange"
-    "DarkOrange"
-    "RedOrange"
-    "BrightRed"
-    "DeepBlue"
-    "codegreen"
-    "codegray"
-    "codepurple"
-    "backcolour"))
+    "eq:4"
+    "eq:2"
+    "eq:5"
+    "sec:konklusion"))
  :latex)
 
